b4012c7216baef06ed821c01b12d64f96e560bd0
James Hilts <jameshilts@icloud.com>
2022-03-20 16:19:29 -0700

    Added the trnasporter, mail options objects and send mail function.

--- controllers/checkEmail.js
+++ controllers/checkEmail.js
@@ -1,4 +1,7 @@
+//To send confirmation email.
+const nodemailer = require('nodemailer')
 
+//Check the database for duplicate email.
 const checkEmail =(req, res, pg, bcrypt)=> {
 	
 	const {email} = req.body
@@ -6,9 +9,13 @@ const checkEmail =(req, res, pg, bcrypt)=> {
 	pg('users')
 	.select('email')
 	.where({email: email})
-	.then(email => {
+	.then(email => {		
+		// If there is no email then the insertUser function is called.
 		!email[0] ? insertUser(req, res, pg, bcrypt)
 		:
+		//If there is a duplicate email then the user exist.
+		//The response would be to reset password on front end
+		//and the response will be the existing email.
 		res.json(email[0])
 	})
 	.catch(error => {
@@ -16,12 +23,16 @@ const checkEmail =(req, res, pg, bcrypt)=> {
 	})
 }
 
+//Insert the user to the database.
 const insertUser =(req, res, pg, bcrypt)=> {
 
-	const {first_name, last_name, email, password} = req.body	
+	const {first_name, last_name, email, password} = req.body
+
+	//Encrypt the user's password for extra security.	
 	const encrypted = bcrypt.hashSync(password)
 
 	pg('users')
+	.returning(['email','id'])
 	.insert({
 		first_name: first_name,
 		last_name: last_name,
@@ -30,10 +41,39 @@ const insertUser =(req, res, pg, bcrypt)=> {
 		date_created: new Date()
 	})
 	.then(user => {
-		res.status(200).json('User created.')
+		//After the insert the emailConfirmation function will be called next.
+		emailConfirmation(req, res, pg, email)
+		//If the insert is successful then a response with the email and id 
+		//will be sent to the fron end as user.
+		res.json(user[0])
 	})
-	.catch(error => {
-		res.status(400).json('Could not insert user.')
-	})
+	.catch(error => Promise.reject(error))
 }
+//Send the email confirmation to the user
+const emailConfirmation =(req, res, pg, email)=> {
+  //Email transporter is where the email is being sent from.
+  //This is an example using ethereal as a test service
+  const transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'toby.watsica42@ethereal.email',
+    pass: 'axYWGsraBjEExwD82C'
+  }
+})
+ //Mail options to set up the actual message being sent. 
+ mailOptions = {
+  from: 'toby.watsica42@ethereal.email',
+  to: email,
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+}
+//Finally send the mail.
+transporter.sendMail(mailOptions, (error, info)=> {
+  	error ? console.log(error)
+  	:
+    console.log('Email sent: ' + info.response)
+})
+}
+
 module.exports = {checkEmail: checkEmail}

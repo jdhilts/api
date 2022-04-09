a888d71a0128cf92e85b31fa0025856b0022ef85
James Hilts <jameshilts@icloud.com>
2022-04-09 15:50:58 -0700

    adding database environment variable to knexfile.js for heroku

--- knexfile.js
+++ knexfile.js
@@ -3,17 +3,17 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
  module.exports = {
-  development: {
-    client: 'pg',
-    version: '8.7.3',
-    connection: {
-      host : '127.0.0.1',
-      user : 'jameshilts',
-      password : '',
-      database : 'users_testdb',
-      charset: 'utf8'
-    }
-  },
+  // development: {
+  //   client: 'pg',
+  //   version: '8.7.3',
+  //   connection: {
+  //     host : '127.0.0.1',
+  //     user : 'jameshilts',
+  //     password : '',
+  //     database : 'users_testdb',
+  //     charset: 'utf8'
+  //   }
+  // },
   production: {
       connectionString: process.env.DATABASE_URL,
       ssl:{

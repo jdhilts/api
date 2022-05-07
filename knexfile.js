487c119d88657dcae05320122a005989cf3f8a59
James Hilts <jameshilts@icloud.com>
2022-05-07 16:01:30 -0700

    Troubleshooting 400 bad request when posting to heroku.

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,16 @@
 
- module.exports = {
+module.exports = {
   development: {
     client: 'pg',
-    version: '8.7.3',
     connection: {
       host : '127.0.0.1',
       user : 'jameshilts',
       password : '',
-      database : 'users_testdb',
-      charset: 'utf8'
-    }
+      database : 'users_testdb'
   },
   production: {
-      client: 'pg',
-      connection:{
+    client: 'pg',
+    connection:{
       connectionString: process.env.DATABASE_URL,
       ssl:{
         rejectionUnauthorized: false
